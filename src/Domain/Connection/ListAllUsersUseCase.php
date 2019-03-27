113e99613397800425385c317cd4e014dc8c8ec4
Sherif Zaki <sherif.zaki@airangel.com>
2019-03-27 11:39:18 +0200

    Added serializer and fixed apiTokenAuthenticator

---
+++ src/Domain/Connection/ListAllUsersUseCase.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sh_za
+ * Date: 27-Mar-19
+ * Time: 9:49 AM
+ */
+
+namespace App\Domain\Connection;
+
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+class ListAllUsersUseCase
+{
+	private $doctrine;
+
+	public function __construct(ManagerRegistry $doctrine)
+	{
+		$this->doctrine = $doctrine;
+	}
+
+	public function handle(): array
+	{
+		$repository = $this->doctrine->getRepository(User::class);
+
+		return $repository->findAll();
+
+	}
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -21,6 +22,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+	 * @Groups("main")
      */
     private $email;
 
